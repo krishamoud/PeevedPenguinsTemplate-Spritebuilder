439eb042811e3a56883399cbf91f4b4bc41560e2
Kristopher Hamoud <kristopherhamoud@Kristophers-MacBook-Pro.local>
2014-08-09 18:43:06 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Kristopher Hamoud on 8/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,25 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Kristopher Hamoud on 8/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init
+{
+    self = [super init];
+    
+    if(self)
+    {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+
+@end
