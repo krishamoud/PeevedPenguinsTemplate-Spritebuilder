044fae86c6ca529eb74491670cbaf65a4447356d
Kristopher Hamoud <kristopherhamoud@Kristophers-MacBook-Pro.local>
2014-08-09 23:17:24 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -21,8 +21,8 @@
 - (void)startBlinkAndJump
 {
     // the animation manager of each node is stored in the 'animationManager' property
-    CCAnimationManager *animationManager = self.animationManager;
-    // Timelines can be referenced and run by name
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
     [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
 }
 
