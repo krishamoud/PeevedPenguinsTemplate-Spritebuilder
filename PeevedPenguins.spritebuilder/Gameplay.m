ccd4f545ffcce1dc9c5dc606ebfd8df16cc85286
Kristopher Hamoud <kristopherhamoud@Kristophers-MacBook-Pro.local>
2014-08-09 20:51:51 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -15,6 +15,8 @@
     CCNode *_levelNode;
     CCNode *_contentNode;
     CCNode *_pullbackNode;
+    CCNode *_mouseJointNode;
+    CCPhysicsNode *_mouseJoint;
 }
 
 //is called when ccb file has completed loading
@@ -30,12 +32,41 @@
     
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
 }
 
 //called on every touch in this scene
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    [self launchPenguin];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    // start catapult dragging when a touch inside of the catapult arm occurs
+    if(CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+    {
+        // move the mouse joint to the touch position
+        _mouseJointNode.position = touchLocation;
+        
+        // setup a spring joint between the mouseJointNode and catapult arm
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
+}
+
+- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    //whenever touches move, update the position of the mouseJointNode to the touch positioin
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
+
+- (void)releaseCatapult
+{
+    if(_mouseJoint != nil)
+    {
+        // releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+        
+    }
 }
 
 - (void)launchPenguin
